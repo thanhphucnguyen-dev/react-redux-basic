be8023dd647244149c0320070879a1ca75ed7ba9
Nguyen Thanh Phuc <thanhphucnguyen54@gmail.com>
2025-05-07 16:19:04 +0700

    Add: server

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,21 @@ import './App.css';
 import Header from './Components/Header/Header';
 import EditPage from './Components/Edit/EditPage';
 import { useState } from 'react';
+import { useSelector } from 'react-redux';
 
 function App() {
 
   const [isEdit, setEdit] = useState(false);
+  const pending = useSelector((state) => state.user.pending);
+  const error = useSelector((state) => state.user.error);
 
   return (
     <div className="App">
         { isEdit ? <EditPage setEdit={setEdit} /> : <Header setEdit={setEdit} /> }  
-        
+        { pending && <p className="loading">Loading...</p>}
+        { !isEdit && error && (
+          <p className="error">Error when fetching user data from server!!!</p>
+        )}
     </div>
   );
 }
